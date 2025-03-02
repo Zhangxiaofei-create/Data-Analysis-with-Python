7d5e66238aaebe6068baba43fb0d8d89bdb06cfd
Zhangxiaofei-create <13682171907@163.com>
2025-03-02 21:27:19 +0800

    Update submission_template01.py

--- Deep Learning/submission_template01.py
+++ Deep Learning/submission_template01.py
@@ -1,15 +1,28 @@
-import numpy as np
 import torch
-from torch import nn
+import torch.nn as nn
+import re
+
+# импортируйте torch и сохраните его версию в переменную version
+# your code here
+version = torch.__version__
+
+# не изменяйте код в блоке ниже! Он нужен для проверки правильности вашего кода.
+# __________start of block__________
+assert version is not None, 'Версия PyTorch не сохранилась в переменную version'
+major_version, minor_version = re.findall("\d+\.\d+", version)[0].split('.')
+assert float(major_version) >= 2 or (float(major_version) >= 1 and float(minor_version) >= 7), 'Нужно обновить PyTorch'
+# __________end of block__________
 
 def create_model():
-    # your code here
-    # return model instance (None is just a placeholder)
-
-    return None
-
-def count_parameters(model):
-    # your code here
-    # return integer number (None is just a placeholder)
-    
-    return None
+    """
+    Создает нейронную сеть из трех линейных слоев с ReLU в качестве функций активации
+    между слоями.
+    """
+    model = nn.Sequential(
+        nn.Linear(784, 256, bias=True),
+        nn.ReLU(),
+        nn.Linear(256, 16, bias=True),
+        nn.ReLU(),
+        nn.Linear(16, 10, bias=True)
+    )
+    return model
