3dc79dc501ea6aab8f6d6f86ccf56888a4c854a9
Zhangxiaofei-create <13682171907@163.com>
2025-03-02 21:38:56 +0800

    Update submission_template01.py

--- Deep Learning/submission_template01.py
+++ Deep Learning/submission_template01.py
@@ -1,15 +1,28 @@
-import numpy as np
 import torch
-from torch import nn
-
-def create_model():
-    # your code here
-    # return model instance (None is just a placeholder)
-
-    return None
+import torch.nn as nn
 
 def count_parameters(model):
-    # your code here
-    # return integer number (None is just a placeholder)
-    
-    return None
+    """
+    Подсчитывает количество параметров в модели.
+
+    Args:
+        model: Модель PyTorch.
+
+    Returns:
+        int: Количество параметров в модели.
+    """
+    total_params = 0
+    for param in model.parameters():
+        total_params += param.numel()
+    # верните количество параметров модели model
+    return total_params
+
+# не изменяйте код в блоке ниже! Он нужен для проверки правильности вашего кода.
+# __________start of block__________
+small_model = nn.Linear(128, 256)
+assert count_parameters(small_model) == 128 * 256 + 256, 'Что-то не так, количество параметров неверное'
+
+medium_model = nn.Sequential(*[nn.Linear(128, 32, bias=False), nn.ReLU(), nn.Linear(32, 10, bias=False)])
+assert count_parameters(medium_model) == 128 * 32 + 32 * 10, 'Что-то не так, количество параметров неверное'
+print("Seems fine!")
+# __________end of block__________
