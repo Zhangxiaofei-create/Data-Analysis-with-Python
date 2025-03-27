1678ebb129665bbe2911307cf2fd80ede40dddd1
Zhangxiaofei-create <13682171907@163.com>
2025-03-27 23:29:49 +0800

    Update submission_template01.py

--- Deep Learning/submission_template01.py
+++ Deep Learning/submission_template01.py
@@ -1,15 +1,145 @@
 import numpy as np
+
 import torch
 from torch import nn
+from torch.nn import functional as F
 
-def create_model():
-    # your code here
-    # return model instance (None is just a placeholder)
+import torchvision
+from torchvision import datasets, transforms
 
-    return None
+from matplotlib import pyplot as plt
+from IPython.display import clear_output
 
-def count_parameters(model):
-    # your code here
-    # return integer number (None is just a placeholder)
-    
-    return None
+# Загрузка данных
+train_data = datasets.CIFAR10(root="./cifar10_data", train=True, download=True, transform=transforms.ToTensor())
+test_data = datasets.CIFAR10(root="./cifar10_data", train=False, download=True, transform=transforms.ToTensor())
+
+train_size = int(len(train_data) * 0.8)
+val_size = len(train_data) - train_size
+
+train_data, val_data = torch.utils.data.random_split(train_data, [train_size, val_size])
+
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_data, batch_size=64, shuffle=False)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False)
+
+# Объявление модели
+class ConvNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Определение слоев сети
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
+        self.bn1 = nn.BatchNorm2d(6)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+        self.conv2 = nn.Conv2d(6, 12, kernel_size=3)
+        self.bn2 = nn.BatchNorm2d(12)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.flatten = nn.Flatten()
+
+        self.fc1 = nn.Linear(12 * 5 * 5, 120)
+        self.bn3 = nn.BatchNorm1d(120)
+        self.fc2 = nn.Linear(120, 10)
+
+    def forward(self, x):
+        # Forward pass сети
+        x = self.pool1(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool2(F.relu(self.bn2(self.conv2(x))))
+        x = self.flatten(x)
+        x = F.relu(self.bn3(self.fc1(x)))
+        x = self.fc2(x)
+        return x
+
+model = ConvNet()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+# Выбор функции потерь и алгоритма оптимизации
+loss_fn = torch.nn.CrossEntropyLoss()
+learning_rate = 1e-3
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Обучение модели
+def evaluate(model, dataloader, loss_fn):
+
+    losses = []
+
+    num_correct = 0
+    num_elements = 0
+
+    for i, batch in enumerate(dataloader):
+
+        # так получаем текущий батч
+        X_batch, y_batch = batch
+        num_elements += len(y_batch)
+
+        with torch.no_grad():
+            logits = model(X_batch.to(device))
+
+            loss = loss_fn(logits, y_batch.to(device))
+            losses.append(loss.item())
+
+            y_pred = torch.argmax(logits, dim=1)
+
+            num_correct += torch.sum(y_pred.cpu() == y_batch)
+
+    accuracy = num_correct / num_elements
+
+    return accuracy.numpy(), np.mean(losses)
+
+def train(model, loss_fn, optimizer, n_epoch=3):
+
+    # цикл обучения сети
+    for epoch in range(n_epoch):
+
+        print("Epoch:", epoch+1)
+
+        model.train(True)
+
+        running_losses = []
+        running_accuracies = []
+        for i, batch in enumerate(train_loader):
+            # так получаем текущий батч
+            X_batch, y_batch = batch
+
+            # forward pass (получение ответов на батч картинок)
+            logits = model(X_batch.to(device))
+
+            # вычисление лосса от выданных сетью ответов и правильных ответов на батч
+            loss = loss_fn(logits, y_batch.to(device))
+            running_losses.append(loss.item())
+
+            loss.backward() # backpropagation (вычисление градиентов)
+            optimizer.step() # обновление весов сети
+            optimizer.zero_grad() # обнуляем веса
+
+            # вычислим accuracy на текущем train батче
+            model_answers = torch.argmax(logits, dim=1)
+            train_accuracy = torch.sum(y_batch == model_answers.cpu()) / len(y_batch)
+            running_accuracies.append(train_accuracy)
+
+            # Логирование результатов
+            if (i+1) % 100 == 0:
+                print("Средние train лосс и accuracy на последних 50 итерациях:",
+                      np.mean(running_losses), np.mean(running_accuracies), end='\n')
+
+        # после каждой эпохи получаем метрику качества на валидационной выборке
+        model.train(False)
+
+        val_accuracy, val_loss = evaluate(model, val_loader, loss_fn=loss_fn)
+        print("Эпоха {}/{}: val лосс и accuracy:".format(epoch+1, n_epoch,),
+                      val_loss, val_accuracy, end='\n')
+
+    return model
+
+model = train(model, loss_fn, optimizer, n_epoch=20)
+
+# Получение метрики качества на тестовой выборке
+test_accuracy, _ = evaluate(model, test_loader, loss_fn)
+print('Accuracy на тесте', test_accuracy)
+
+# Сохранение модели
+model.eval()
+x = torch.randn((1, 3, 32, 32))
+torch.jit.save(torch.jit.trace(model.cpu(), (x)), "model.pth")
